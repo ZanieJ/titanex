fe73d596371da1a071952964205d4182e6399905
ZanieJ <zaniej83@gmail.com>
2025-08-10 10:08:32 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './style.css'
+import { createRoot } from 'react-dom/client'
+import App from './app.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
