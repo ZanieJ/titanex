ae2f6bcbe3fa76519c6134c714def831de55bd0c
ZanieJ <zaniej83@gmail.com>
2025-08-10 11:41:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default {
   plugins: [react()],
   build: {
-    target: 'es2020'
+    rollupOptions: {
+      input: "./index.html"
+    }
   }
-})
+};
