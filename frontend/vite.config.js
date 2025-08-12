46a17fe52fc364376078ca8687d1150656b2266b
ZanieJ <zaniej83@gmail.com>
2025-08-12 15:54:12 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [] // No need for pdfjs-dist here — it's from CDN
+    }
+  }
 });
