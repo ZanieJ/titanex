a11ebe00d6f0994716a79dd80d635d6357528fca
ZanieJ <zaniej83@gmail.com>
2025-08-12 13:11:48 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['pdfjs-dist/build/pdf']
+  },
   build: {
     rollupOptions: {
       external: ['tesseract.js']
